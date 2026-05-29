53ac03474c57fcc8bba7f7e6433fa55240f2410b
Luz Agudelo Palacio <tatgupa@hotmail.com>
2026-05-28 20:26:21 -0500

    Add files via upload

---
+++ municipio.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM vivienda.municipio
+LIMIT 0, 50000
+
+-- Date: 2026-04-09 21:36
+*/
+INSERT INTO `` (`cod_muni`,`Municipio`,`Departamento`) VALUES ('051','Medellin','Antioquia');
+INSERT INTO `` (`cod_muni`,`Municipio`,`Departamento`) VALUES ('081','Barranquilla','Atlantico');
+INSERT INTO `` (`cod_muni`,`Municipio`,`Departamento`) VALUES ('111','Bogota','Bogota');
+INSERT INTO `` (`cod_muni`,`Municipio`,`Departamento`) VALUES ('131','Cartagena','Bolivar');
+INSERT INTO `` (`cod_muni`,`Municipio`,`Departamento`) VALUES ('761','Cali','Valle del Cauca');
